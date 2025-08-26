6e50bf4332c279d3a8b6bbc03def751f58f7d1c3
YagraProk <zssl@mail.ru>
2025-08-26 19:46:13 +0700

    price&mail

--- src/price.js
+++ src/price.js
@@ -1,3 +1,29 @@
-document.querySelector('.contacts_btn').addEventListener('click', () => {
-    document.querySelector('.drop_contacts').classList.toggle('_active')
-})
+document.querySelector('.contacts_btn').addEventListener('click', (e) => {
+    e.stopPropagation();
+    document.querySelector('.drop_contacts').classList.toggle('_active');
+});
+
+document.addEventListener('click', (e) => {
+    const dropContacts = document.querySelector('.drop_contacts');
+    const contactsBtn = document.querySelector('.contacts_btn');
+    
+    const isClickInsideMenu = dropContacts.contains(e.target);
+    const isClickOnButton = contactsBtn.contains(e.target);
+    
+    if (!isClickInsideMenu && !isClickOnButton && dropContacts.classList.contains('_active')) {
+        dropContacts.classList.remove('_active');
+    }
+});
+
+
+document.addEventListener('touchstart', (e) => {
+    const dropContacts = document.querySelector('.drop_contacts');
+    const contactsBtn = document.querySelector('.contacts_btn');
+    
+    const isTouchInsideMenu = dropContacts.contains(e.target);
+    const isTouchOnButton = contactsBtn.contains(e.target);
+    
+    if (!isTouchInsideMenu && !isTouchOnButton && dropContacts.classList.contains('_active')) {
+        dropContacts.classList.remove('_active');
+    }
+});
