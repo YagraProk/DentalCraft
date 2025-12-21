5c46c822a37741c81945582b8160900597850087
YagraProk <zssl@mail.ru>
2025-12-21 22:00:48 +0700

    telegram resize gnatphoto

--- src/whatsapbtn.js
+++ src/whatsapbtn.js
@@ -1,30 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const telegramButton = document.querySelector('.telegram_button');
   const whatsappButton = document.querySelector('.whatsap_button');
   const closeButton = document.querySelector('.close-button');
+  
+ 
+  function showBothIcons() {
+    whatsappButton.classList.remove('hidden');
+    telegramButton.classList.remove('hidden');
+  }
   
   
   closeButton.addEventListener('click', function(e) {
     e.preventDefault();
     e.stopPropagation();
     whatsappButton.classList.add('hidden');
+    telegramButton.classList.add('hidden');
   });
   
-  
+ 
   whatsappButton.addEventListener('click', function(e) {
     if (whatsappButton.classList.contains('hidden')) {
       e.preventDefault();
       e.stopPropagation();
-      whatsappButton.classList.remove('hidden');
+      showBothIcons();
     }
   });
-  
-  
-  const whatsappLink = document.querySelector('.whatsap_button a');
-  whatsappLink.addEventListener('click', function(e) {
-    if (whatsappButton.classList.contains('hidden')) {
+
+ 
+  telegramButton.addEventListener('click', function(e) {
+    if (telegramButton.classList.contains('hidden')) {
       e.preventDefault();
       e.stopPropagation();
-      whatsappButton.classList.remove('hidden');
+      showBothIcons();
     }
   });
 });
